fb7b0ff27ccb1dbdee2a92b01654500119ed82d8
Shirish Hadpad <Hadp_Shi@KlingelnbergGmbH.onmicrosoft.com>
2024-06-10 18:51:15 +0530

    code refactor

--- ProductInformation.API/Controllers/ReportsController.cs
+++ ProductInformation.API/Controllers/ReportsController.cs
@@ -3,8 +3,6 @@
 namespace WebApplication1.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
-    using System.Net;
-    using System.Net.Mail;
     using Telerik.Reporting.Services;
     using Telerik.Reporting.Services.AspNetCore;
 
